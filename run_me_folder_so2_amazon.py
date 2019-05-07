6873f5c134cc716506db1067bea5960bbf5b3da6
Ubuntu <ubuntu@ip-172-31-45-104.us-west-2.compute.internal>
2019-05-07 18:40:23 +0000

    from_mac

--- run_me_folder_so2_amazon.py
+++ run_me_folder_so2_amazon.py
@@ -14,28 +14,27 @@
 
 # %%
 import util
-
 import importlib
 importlib.reload(util);
 
 # secret_conf.secret_conf is the location of the secret_conf file. u can have
 # multiple ones, just edit the following lines
-import secret_conf.secret_conf as secret_conf
+from secret_conf import secret_conf
 # %%
 conf_dic = dict(
-    monitor_mode= 'connected', # options: folder, connected, value
+    monitor_mode= 'folder', # options: folder, connected, value
     from_addr    = '', # this values is not shown anywhere as of now
     to_addr_list = secret_conf.secret_dic['to_addr_list'],
     cc_addr_list = secret_conf.secret_dic['cc_addr_list'],
-    subject      = 'so2 analyzer at chc not connected to internet',
+    subject      = 'so2 sync folder at amazon not increasing',
     message      = '',
     login        = secret_conf.secret_dic['login'],
     password     = secret_conf.secret_dic['password'],
-    watch_path   = '/tmp/data', # directory that needs to be increasing. mode: folder
+    watch_path   = '/uhel_data/so2_chc', # directory that needs to be increasing. mode: folder
     host         = '10.8.3.33', # in case connection test, this is the address of the monitored computer. mode connected
     port         = 2105, # this is the port that we will check is open in the monitored computer. mode connected
-    time_interval= 60, #seconds 
-    tries        = 10, # tries before sending email (time_interval*tries)
+    time_interval= 600, #seconds 
+    tries        = 3, # tries before sending email (time_interval*tries)
     value_range  = [0, 10], # the value range to be monitored. only needed for mode value
     value_func   = None # function to get the value_range
 
