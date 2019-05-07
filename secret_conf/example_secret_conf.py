4eb6d436c1aaf1f87d42f9064549a862719ac05b
diego <diego.aliaga@helsinki.fi>
2019-05-07 14:20:14 -0400

    from_mac

--- secret_conf/example_secret_conf.py
+++ secret_conf/example_secret_conf.py
@@ -1,8 +1,12 @@
 # project name: py_data_watchdog
 # created by diego aliaga daliaga_at_chacaltaya.edu.bo
-
+'''
+this file must be copied as secret_conf.py in the same folder. And update the values.
+note that the file secret_conf.py will not be tracked by git or github for security
+reasons
+'''
 secret_dic = dict(
-    to_addr_list = ['too@mail.com'],
+    to_addr_list = ['to@mail.com'],
     cc_addr_list = [],
     login        = 'from@mail.com',
     password     = 'pass',
